a45f059a978f5b70f90f68a1e10a76b208fb7d8d
lorenzosavoia <lorenzo.savoia97@gmail.com>
2021-12-01 18:46:42 +0100

    end

--- js/script.js
+++ js/script.js
@@ -1,15 +1,64 @@
-const row = 10;
-const col = 10;
-const numberSquare = row * col;
-const containerGrid = document.getElementById('grid');
+let button = document.querySelector('.bottone');
+button.addEventListener('click', function () {
+    let level = document.getElementById("difficulty");
+    let active = document.querySelector(".active");
+    let containerGrid = document.getElementById('grid');
 
-for (let i = 0; i < numberSquare; i++) {
 
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = `calc(100% / ${col})`
-    square.style.height = `calc(100% / ${row})`
-    square.append([i + 1])
-    containerGrid.append(square);
-    
-}
+    if (level.value == '1') {
+
+        containerGrid.innerHTML = "";
+        active.classList.add("show");
+        let row = 10;
+        let col = 10;
+        let numberSquare = row * col;
+
+        for (let i = 0; i < numberSquare; i++) {
+
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `calc(100% / ${col})`
+            square.style.height = `calc(100% / ${row})`
+            square.append([i + 1])
+            containerGrid.append(square);
+
+        }
+
+    } else if (level.value == '2') {
+
+        containerGrid.innerHTML = "";
+        active.classList.add("show");
+        let row = 9;
+        let col = 9;
+        let numberSquare = row * col;
+
+        for (let i = 0; i < numberSquare; i++) {
+
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `calc(100% / ${col})`
+            square.style.height = `calc(100% / ${row})`
+            square.append([i + 1])
+            containerGrid.append(square);
+
+        }  
+    } else {
+        containerGrid.innerHTML = "";
+        active.classList.add("show");
+        let row = 7;
+        let col = 7;
+        let numberSquare = row * col;
+
+        for (let i = 0; i < numberSquare; i++) {
+
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `calc(100% / ${col})`
+            square.style.height = `calc(100% / ${row})`
+            square.append([i + 1])
+            containerGrid.append(square);
+
+        }
+    }
+})
+
